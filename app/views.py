01a39e62fd4b1c905c6c2a1c07e5644f2b57b9ad
Anders Bogsnes <andersbogsnes@gmail.com>
2016-11-18 12:07:01 +0100

    Added main.css for custom styling Added navbar in header Added sidebar Added main posting area

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 from flask import render_template
 from app import app
 
+posts = [{'content': "Test post 1"}, {'content': "Test post 2"}]
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', posts=posts)
