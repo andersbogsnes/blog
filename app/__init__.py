05502f331bcef29d95aaf10f080baf49525711d7
Anders Bogsnes <andersbogsnes@gmail.com>
2016-11-27 11:37:03 +0100

    Tweaked config

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
-app.config.from_pyfile('config.py')
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 db = SQLAlchemy(app)
 
